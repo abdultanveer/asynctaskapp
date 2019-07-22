fb0c24003488aa37cf78e472f5545e2e0ffffb47
abdul <abdul.tanveer@gmail.com>
2019-07-22 19:31:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asynctaskapp/DownloadTask.java
@@ -0,0 +1,58 @@
+package com.example.asynctaskapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class DownloadTask extends AsyncTask<String ,Integer,Boolean > {
+    ProgressBar progressBar;
+    public static String TAG = DownloadTask.class.getSimpleName();
+
+
+    public DownloadTask(ProgressBar mProgressBar) {
+        progressBar = mProgressBar;
+    }
+    //input type = url -- string
+    //progress type = %downloaded -- integer
+    //result type -- Boolean
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    //this method will run on the seperate bg thread
+    @Override
+    protected Boolean doInBackground(String... url) {
+        Log.i(TAG,"url"+url[0]);
+        int totalSecs = 100;
+        for(int i=0; i<totalSecs;i++){
+            try {
+                Thread.sleep(300);
+                i += 5;  //i = i+10
+                publishProgress(i); //onProgressUpdate
+                //i am sending a progress[%age downloaded] to the ui thread
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return true;//onPostExecute
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        progressBar.setProgress(values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+        progressBar.setVisibility(View.GONE);
+    }
+}
---
+++ app/src/main/java/com/example/asynctaskapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.asynctaskapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar mProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mProgressBar = findViewById(R.id.progressBar1);
+    }
+
+    public void handleClick(View view) {
+        DownloadTask downloadTask = new DownloadTask(mProgressBar);
+        downloadTask.execute("http://file url to download");//onPreExecute
+    }
+}
